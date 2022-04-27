b39626d02d90993a8cee625e333daef000575338
Dipesh Kumar Mandal <dipeshmandal160@gmail.com>
2022-04-27 15:44:39 +0545

    Updates

---
+++ precache-manifest.442fed6684ccec4ad0f245063f4bba24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed965d9be3899b2b1d41196143623052",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "cb2e800c41ebf440fdda",
+    "url": "/Movie-App/static/css/main.db3b9a05.chunk.css"
+  },
+  {
+    "revision": "7282008379199bd48ee9",
+    "url": "/Movie-App/static/js/2.a49e5830.chunk.js"
+  },
+  {
+    "revision": "50f4a02157d83cc5f2d4",
+    "url": "/Movie-App/static/js/3.09a0fe09.chunk.js"
+  },
+  {
+    "revision": "cb2e800c41ebf440fdda",
+    "url": "/Movie-App/static/js/main.8eaaf142.chunk.js"
+  },
+  {
+    "revision": "90709ad3e1ff6338543f",
+    "url": "/Movie-App/static/js/runtime~main.c2864a4f.js"
+  }
+]);
